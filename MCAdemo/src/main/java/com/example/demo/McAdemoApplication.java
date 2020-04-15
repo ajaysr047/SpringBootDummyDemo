e7f9de1393a5e4b3604bf88ddd14d51a6daa951b
Ajay S <41289781+ajaysr047@users.noreply.github.com>
2020-04-15 21:51:40 +0530

    Revert "Revert "Initial Commit""
    This reverts commit ee90d579fc28df912d0c80cfffbbc00628e5b366.

---
+++ MCAdemo/src/main/java/com/example/demo/McAdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McAdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McAdemoApplication.class, args);
+	}
+
+}
